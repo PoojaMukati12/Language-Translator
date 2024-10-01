00bde6b1c8b6045f14b91002cf2f1cd373f84432
PoojaMukati12 <poojamukku424@gmail.com>
2024-10-01 11:21:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Translate from './Component/Translate'
+import './Media.css'
+
+function App() {
+ 
+
+  return (
+    <> 
+        <Translate/>
+    </>
+  )
+}
+
+export default App
